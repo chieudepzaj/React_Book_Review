34a408c9ddcf35aaa4b304dd32f3f0cf3d95d14e
EC2 Default User <ec2-user@ip-172-31-6-115.ec2.internal>
2021-05-29 10:01:29 +0000

    change dev team

--- src/components/Footers/FooterSmall.js
+++ src/components/Footers/FooterSmall.js
@@ -17,10 +17,10 @@ export default function FooterSmall(props) {
               <div className="text-sm text-blueGray-500 font-semibold py-1 text-center md:text-left">
                 Copyright © {new Date().getFullYear()}{" "}
                 <a
-                  href="https://www.creative-tim.com?ref=nr-footer-small"
+                  href="#"
                   className="text-white hover:text-blueGray-300 text-sm font-semibold py-1"
                 >
-                  Creative Tim
+                  Team_03_Hedspi
                 </a>
               </div>
             </div>
@@ -44,7 +44,7 @@ export default function FooterSmall(props) {
                 </li>
                 <li>
                   <a
-                    href="http://blog.creative-tim.com?ref=nr-footer-small"
+                    href="#"
                     className="text-white hover:text-blueGray-300 text-sm font-semibold block py-1 px-3"
                   >
                     Blog
