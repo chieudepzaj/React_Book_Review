c0f70860c4329643b9f2f8c9a8070468c1c60e2e
quangnm2405 <quangnm.vnist@gmail.com>
2021-06-09 20:46:41 +0700

    auth function

--- src/layouts/Auth.js
+++ src/layouts/Auth.js
@@ -11,7 +11,7 @@ import FooterSmall from "components/Footers/FooterSmall.js";
 import Login from "views/auth/Login.js";
 import Register from "views/auth/Register.js";
 
-export default function Auth() {
+function Auth() {
   return (
     <>
       <Navbar transparent />
@@ -35,3 +35,5 @@ export default function Auth() {
     </>
   );
 }
+
+export default Auth;
